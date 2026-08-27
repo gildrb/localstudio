fe7d0a06269a894824cad98d3e94c48b8de2e517
Gil Rodrigues <hi@gildrb.com>
2026-08-28 00:14:52 +0200

    fix(ci): handle knip workspace self-binary

--- controller/knip.ts
+++ controller/knip.ts
@@ -1,6 +1,7 @@
 export default {
   project: ["src/**/*.ts"],
   ignore: ["bun.lockb", "node_modules/**", "dist/**", "src/**/index.ts"],
+  ignoreBinaries: ["knip"],
   ignoreExportsUsedInFile: true,
   ignoreWorkspaces: [],
 };
